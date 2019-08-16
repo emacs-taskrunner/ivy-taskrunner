576a0d328c29629cfa73d1da9b169b12d6b048ed
Yavor Konstantinov <ykonstantinov1@gmail.com>
2019-08-15 21:26:48 -0700

    Add package docs to source code

--- ivy-taskrunner.el
+++ ivy-taskrunner.el
@@ -1,7 +1,73 @@
-;; Test interface for taskrunner
+;;; ivy-taskrunner.el --- Retrieve build system/taskrunner tasks via ivy -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019 Yavor Konstantinov
+
+;; Author: Yavor Konstantinov <ykonstantinov1 AT gmail DOT com>
+;; URL: https://github.com/emacs-taskrunner/ivy-taskrunner
+;; Version: 1.0
+;; Package-Requires: ((emacs "24"))
+;; Keywords: build-system taskrunner build task-runner tasks ivy
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+;; This package provides an ivy interfaces to the taskrunner library
+
+;;;; Installation
+
+;;;;; MELPA
+;; If installed form MELPA then you are done.
+
+;;;;; Manual
+
+;; Install these required packages:
+
+;; projectile
+;; taskrunner
+;; ivy
+
+;; Then put this folder in your load-path, and put this in your init:
+
+;; (require 'ivy-taskrunner)
+
+;;;; Usage
+
+;; When in any buffer, either call the command `ivy-taskrunner' or
+;; `ivy-taskrunner-update-cache' to be presented with a list of targets/tasks in
+;; your project
+
+;;;; Credits
+
+;; This package would not have been possible without the following
+;; packages:
+;; ivy.el[1] which helped me create the interface
+;;
+;; [1] https://github.com/abo-abo/swiper
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+;;;; Requirements
+
 
 (require 'ivy)
 (require 'taskrunner)
+
+;;;; Variables
 
 ;; Users can add additional actions by appending to this variable
 (defvar ivy-taskrunner-actions
@@ -11,6 +77,8 @@
     ("C" ivy-taskrunner--current-dir-prompt "Run task in current folder with args")
     )
   "A list of extra actions which can be used when running a task selected through ivy.")
+
+;;;; Functions
 
 (defun ivy-taskrunner--root-task (TASK)
   "Run the task TASK in the project root without asking for extra args.
